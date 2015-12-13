f2646bdecd2ca3018bb90297176e3e6011def622
Traci Cremeans <icartusacrimea@gmail.com>
2015-12-13 23:17:40 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213220756) do
+ActiveRecord::Schema.define(version: 20151213231426) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
